36a5dced9e2868bc8eda82803e5104b98378dd54
GERARDO MORALES <moralesgerardomgz@outlook.com>
2021-08-01 21:47:00 -0500

    Se agregar correciones en el modulo de controladorFactura

--- src/main/kotlin/com/example/ioc_spring/modelo/Factura.kt
+++ src/main/kotlin/com/example/ioc_spring/modelo/Factura.kt
@@ -5,11 +5,15 @@ import lombok.Getter
 import lombok.Setter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import org.springframework.web.context.annotation.RequestScope
 
 @Component
+@RequestScope
 @Getter
 @Setter
 class Factura {
+    //@RequestScope mantiene la intancia hata que se destruye con la peticion HTTP
+
     @Value("\${factura.desc}")
     final var descripcionFactura: String? = null
 
